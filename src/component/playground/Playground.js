83bbbac21e1a6e032131735f4abd7eb67f23747f
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2023-06-03 16:23:36 -0700

    edit inital setup

--- src/component/playground/Playground.js
+++ src/component/playground/Playground.js
@@ -1,12 +1,24 @@
 import ReactDOMServer from 'react-dom/server';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { html } from 'js-beautify';
 import TemplateOne from '../templates/TemplateOne';
+import "../playground/playground.css"
 
 const Playground = () => {
-  const [templateContent, setTemplateContent] = useState(ReactDOMServer.renderToStaticMarkup(<TemplateOne />));
+  const [rawContent, setRawContent] = useState(ReactDOMServer.renderToStaticMarkup(<TemplateOne />));
+  const [templateContent, setTemplateContent] = useState(html(rawContent, { indent_size: 2 }));
+
+  useEffect(() => {
+    document.getElementById('src-code').value = templateContent;
+  }, []);
 
   const handleInputChange = (event) => {
-    setTemplateContent(event.target.value);
+    setRawContent(event.target.value);
+  };
+
+  const handleInputBlur = () => {
+    const prettyHtml = html(rawContent, { indent_size: 2 });
+    setTemplateContent(prettyHtml);
   };
 
   const handleSaveTemplate = () => {
@@ -16,18 +28,35 @@ const Playground = () => {
 
   return (
     <div className="playground">
-      <div className="textarea-container">
-        <textarea value={templateContent} onChange={handleInputChange} />
-        <div className="template-preview" dangerouslySetInnerHTML={{ __html: templateContent }}></div>
+    <div className="textarea-container">
+      <div className="row">
+        <textarea 
+          id="src-code" 
+          className="src-code" 
+          value={rawContent} 
+          onChange={handleInputChange}
+          onBlur={handleInputBlur} 
+        />
 
-        
+
+        <iframe 
+          className="template-preview" 
+          srcDoc={templateContent}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '1rem',
+            border: '10px solid',
+            backgroundColor: 'rgba(255, 255, 255)'
+          }}
+        />
       </div>
-
-      <button onClick={handleSaveTemplate}>Save Template</button>
-
     </div>
-  );
-  
+
+    <button onClick={handleSaveTemplate}>Save Template</button>
+  </div>
+);
 };
 
 export default Playground;
--- src/component/templates/TemplateOne.js
+++ src/component/templates/TemplateOne.js
@@ -4,28 +4,38 @@ const TemplateOne = () => {
   return (
     <div className="template-page">
       <header>
-        <h1>Welcome to the Template Page</h1>
+        <h1 contentEditable>Hello, I'm John Doe</h1>
+        <p contentEditable>I'm a Music Designer</p>
       </header>
       <main>
         <section>
-          <h2>About</h2>
-          <p>This is a template static webpage.</p>
+          <h2 contentEditable>About Me</h2>
+          <p contentEditable>
+            I am a passionate Music Designer with over 5 years of experience in creating compelling soundtracks for various multimedia projects.
+            I specialize in music composition, sound design, and audio post-production.
+          </p>
         </section>
         <section>
-          <h2>Features</h2>
+          <h2 contentEditable>My Skills</h2>
           <ul>
-            <li>Responsive design</li>
-            <li>Easy customization</li>
-            <li>Interactive components</li>
+            <li contentEditable>Music Composition</li>
+            <li contentEditable>Sound Design</li>
+            <li contentEditable>Audio Post-Production</li>
           </ul>
         </section>
         <section>
-          <h2>Contact</h2>
-          <p>For any inquiries, please contact us.</p>
+          <h2 contentEditable>My Work</h2>          
+          <div contentEditable>Soundtrack for the video game "Epic Adventure"</div>
+          <div contentEditable>Original score for the short film "Silent Nights"</div>
+          <div contentEditable>Sound design for the mobile game "Mystery Manor"</div>
+        </section>
+        <section>
+          <h2 contentEditable>Contact</h2>
+          <p contentEditable>For any inquiries, please contact me at john.doe@email.com</p>
         </section>
       </main>
       <footer>
-        <p>&copy; 2023 Your Company</p>
+        <p contentEditable>&copy; 2023 John Doe</p>
       </footer>
     </div>
   )
