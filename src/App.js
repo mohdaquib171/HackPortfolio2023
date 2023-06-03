122e1a3e832a21612f4e46cdf3d381e637340235
Renu Prajapati <prajapatirenu790@gmail.com>
2023-06-03 14:22:08 +0530

    removed unnecessary lines (error resolved)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Navbar from "./component/navbar/Navbar";
 import Home from "./component/home/Home";
-<<<<<<< HEAD
 import Login from "./component/authentication/Login";
 import Register from "./component/authentication/Register";
 import Footer from "./component/footer/Footer";
 import Templates from "./component/templates/Templates";
-=======
 import Playground from "./component/playground/Playground";
->>>>>>> 46055d0495fca9460f49b12fb7a4b03214e652a4
 
 function App() {
   return (
@@ -18,16 +15,12 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/"  element={<Home />} /> 
-<<<<<<< HEAD
           <Route path="/login"  element={<Login />} /> 
           <Route path="/register"  element={<Register />} /> 
           <Route path="/templates"  element={<Templates />} /> 
+          <Route path="/playground"  element={<Playground />} />
         </Routes>      
-        <Footer />                                                                        
-=======
-          <Route path="/playground"  element={<Playground />} /> 
-        </Routes>                                                                              
->>>>>>> 46055d0495fca9460f49b12fb7a4b03214e652a4
+        <Footer />                                                                                                                                                      
     </BrowserRouter>
     </>
   );
